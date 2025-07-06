4e3a0b9fb1baf9936ffd143ccdff29c0f583301e
emillyalves <147439821+em1srod@users.noreply.github.com>
2025-07-06 01:47:34 -0300

    chore: adiciona CI com GitHub Actions

---
+++ README.md
@@ -0,0 +1,18 @@
+# Portfólio de Testes Automatizados com Cypress 🧪🚀
+
+![CI](https://github.com/em1srod/potifolio-qa-cypress/actions/workflows/cypress.yml/badge.svg)
+
+Repositório de estudos e demonstração de boas práticas em QA utilizando Cypress + BDD.
+
+- **UI**: https://automationpractice.com  
+- **API**: https://reqres.in  
+- **Cucumber/Gherkin** para legibilidade de negócio  
+- Evidências automáticas (prints & vídeos)  
+- Pipeline GitHub Actions
+
+## Como executar
+
+```bash
+npm install        # instala dependências
+npm run open       # modo interativo
+npm test           # headless
---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumber =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://automationpractice.com",   // alvo UI
+    async setupNodeEvents(on, config) {
+      // habilita Cucumber + esbuild
+      await addCucumber(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+      return config;
+    },
+    // todos os testes .feature ficarão aqui
+    specPattern: "cypress/integration/**/*.feature",
+    screenshotsFolder: "cypress/evidences",
+    videosFolder: "cypress/evidences",
+    video: true,          // gravações automáticas
+  },
+});
